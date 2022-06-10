272268fddc9cb616843c60a38d9f7498ee143b28
annieko33 <ehejrhc@gmail.com>
2022-06-11 07:08:18 +0900

    캐릭터 모션 정의

--- coding/code/js/game.js
+++ coding/code/js/game.js
@@ -4,7 +4,8 @@ const key = {
   keyValue : {
     37: 'left',
     38: 'up',
-    39: 'right'
+    39: 'right',
+    88: 'attack'
   }
 }
 
@@ -12,13 +13,17 @@ const key = {
 const windowEvent = () => {
  window.addEventListener('keydown', e => {
   key.keyDown[key.keyValue[e.which]] = true;
+  hero.keyMotion();
  });
  window.addEventListener('keyup', e => {
   key.keyDown[key.keyValue[e.which]] = false;
+  hero.keyMotion();
 })
 }
 
+let hero;
 const init = () => {
+  hero = new Hero('.hero'); // hero 인스턴스 생성 할당
   windowEvent(); // 윈도우 이벤트
 }
 
