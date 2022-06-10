f03e4dc71e1f29871edae07cb793c2ba81871409
annieko33 <ehejrhc@gmail.com>
2022-06-11 08:01:27 +0900

    이미지 프리로드 및 rAF 정의

--- coding/code/js/class.js
+++ coding/code/js/class.js
@@ -1,14 +1,20 @@
 class Hero {
   constructor(el) {
     this.el = document.querySelector(el);
+    this.moveX = 0;
+    this.speed = 16;
   }
 
   keyMotion(){
     if(key.keyDown['left']){
-      this.el.classList.add('run','flip')
+      this.el.classList.add('run','flip');
+
+      this.moveX = this.moveX - this.speed;
     }else if(key.keyDown['right']){
       this.el.classList.add('run');
       this.el.classList.remove('flip');
+
+      this.moveX = this.moveX + this.speed;
     }
 
     if(key.keyDown['attack']) this.el.classList.add('attack');
@@ -16,5 +22,7 @@ class Hero {
     if(!key.keyDown['left'] && !key.keyDown['right']) this.el.classList.remove('run');
 
     if(!key.keyDown['attack']) this.el.classList.remove('attack');
+
+    this.el.parentNode.style.transform = `translateX(${this.moveX}px)`;
   }
 }
--- coding/code/js/game.js
+++ coding/code/js/game.js
@@ -9,22 +9,39 @@ const key = {
   }
 }
 
+const renderGame = () => {
+  hero.keyMotion();
+  // hero keyMotion에 대한 애니메이션 frame 
+  window.requestAnimationFrame(renderGame);
+}
+
 // window event 관련 함수 작성란
 const windowEvent = () => {
  window.addEventListener('keydown', e => {
   key.keyDown[key.keyValue[e.which]] = true;
-  hero.keyMotion();
  });
  window.addEventListener('keyup', e => {
   key.keyDown[key.keyValue[e.which]] = false;
-  hero.keyMotion();
 })
+}
+
+const loadImg = () => {
+  const imgPath = '../../lib/images'
+  // 사용되어질 이미지 선 로드 작업
+  // 캐릭터의 모션이 키 가 눌렸을때 css의 백그라운드 이미지가 늦게 불리는 경우를 대비
+  const preLoadImgSrc = [imgPath + '/ninja_attack.png', imgPath + '/ninja_run.png'];
+  preLoadImgSrc.forEach(arr=>{
+    const img = new Image();
+    img.src = arr;
+  })
 }
 
 let hero;
 const init = () => {
   hero = new Hero('.hero'); // hero 인스턴스 생성 할당
+  loadImg();
   windowEvent(); // 윈도우 이벤트
+  renderGame();
 }
 
 // 모든 리소스 로드 후 init 실행
