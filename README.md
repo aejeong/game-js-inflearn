652f158ba2d511b4f1876dfd674f8082afd6d8b7
annieko33 <ehejrhc@gmail.com>
2022-06-11 06:26:11 +0900

    코드 키 값 정의

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # game-js-inflearn
  
+
+
+FPS (Frame per second)
+
+requestAnimationFrame
+초당 약 60회를 그림
--- coding/code/js/game.js
+++ coding/code/js/game.js
@@ -1,2 +1,28 @@
-/* 코드를 작성하세요 */
+// 필요한 key 값 정리
+const key = {
+  keyDown : {},
+  keyValue : {
+    37: 'left',
+    38: 'up',
+    39: 'right'
+  }
+}
 
+// window event 관련 함수 작성란
+const windowEvent = () => {
+ window.addEventListener('keydown', e => {
+  key.keyDown[key.keyValue[e.which]] = true;
+ });
+ window.addEventListener('keyup', e => {
+  key.keyDown[key.keyValue[e.which]] = false;
+})
+}
+
+const init = () => {
+  windowEvent(); // 윈도우 이벤트
+}
+
+// 모든 리소스 로드 후 init 실행
+window.onload = () => {
+  init();
+}
